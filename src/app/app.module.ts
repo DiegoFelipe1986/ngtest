ead43b46656d200d422633d52eaadf0443ceade3
Diego pinzon <diegopinzondev@gmail.com>
2022-04-26 16:07:30 -0500

    initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { MainComponent } from './pages/main/main.component';
+import { LiveComponent } from './pages/live/live.component';
+import { FeaturedComponent } from './pages/featured/featured.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HighlightsComponent } from './pages/highlights/highlights.component';
+import { CustomersComponent } from './pages/customers/customers.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    LiveComponent,
+    FeaturedComponent,
+    AboutComponent,
+    HighlightsComponent,
+    CustomersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
